46d44edbe88ab604f4312e486a73296cb346c74b
Jean Rondón <jeanrdev@gmail.com>
2022-09-06 21:09:35 -0400

    end-seccion-2

---
+++ src/clean-code/05-dry.ts
@@ -0,0 +1,48 @@
+
+type Size = '' | 'S' | 'M' | 'XL' | 'L';
+
+class Product {
+    constructor(
+        public name: string = '',
+        public price: number = 0,
+        public size: Size = '',
+    ) { }
+
+    isProductReady(): boolean {
+
+        for (const key in this) {
+
+            switch (typeof this[key]) {
+                case 'string':
+                    if ((<string><unknown>this[key]).length <= 0) throw Error(`${key} is empty`);
+                    break;
+                case 'number':
+                    if ((<number><unknown>this[key]) <= 0) throw Error(`${key} is zero`);
+                    break;
+                default:
+                    throw Error(`${typeof this[key]} is not valid`);
+            }
+
+        }
+
+        return true;
+    }
+
+
+    toString() {
+
+        if (!this.isProductReady) return;
+
+        return `${this.name} (${this.price}), ${this.size}`;
+
+    }
+
+}
+
+(() => {
+
+    const bluePants = new Product('Blue Pants', 102, 'L');
+
+    console.log(bluePants.toString());
+
+})();
